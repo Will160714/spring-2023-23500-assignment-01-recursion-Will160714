4fca5426f12dadf50116eba94c0572a0343ec639
Will160714 <95445527+Will160714@users.noreply.github.com>
2023-02-19 14:40:06 -0500

    Fixed

--- knights.h
+++ knights.h
@@ -1,5 +1,7 @@
 #pragma once
 
 void print(int board[][5]);
-void solve(int board[][5], int x, int y, int counter);
 void clear(int board[][5], int x);
+bool find(int board[][5], int num);
+bool complete(int board[][5]);
+void solve(int board[][5], int x, int y, int counter);
