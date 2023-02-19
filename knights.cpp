6273b86d0b2616e9bcdc9ca413b40165fcfea19e
Will160714 <95445527+Will160714@users.noreply.github.com>
2023-02-19 12:51:39 -0500

    Testing

---
+++ knights.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <unistd.h>
+#include "knights.h"
+
+void print(int board[][5]){
+    //std::cout << "[0;0H\n";
+
+    for(int x = 0; x < 5; x++){
+        for(int y = 0; y < 5; y++){
+            std::cout << board[x][y] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+bool complete(int board[][5]){
+    for(int x = 0; x < 5; x++){
+        for(int y = 0; y < 5; y++){
+            if(board[x][y] == 0)
+                return false;
+        }
+    }
+    return true;
+}
+
+void clear(int board[][5], int z){
+    for(int x = 0; x < 5; x++){
+        for(int y = 0; y < 5; y++){
+            if(board[x][y] >= z)
+                board[x][y] = 0;
+        }
+    }
+}
+
+bool done = false;
+void solve(int board[][5], int x, int y, int counter){
+    if((board[x][y] <= counter && board[x][y] != 0)|| x >= 5 || y >= 5 || x < 0 || y < 0)
+        return;
+    
+    board[x][y] = counter;
+    //usleep(100000);
+    //print(board);
+
+    if(complete(board)){
+        std::cout << std::endl;
+        std::cout << "Solution" << std::endl;
+        print(board);
+        done = true;
+        return;
+    }
+
+    if(!done){
+        solve(board, x - 1, y - 2, counter + 1);
+        if(!done) clear(board, counter + 1);
+    }
+    if(!done){
+        solve(board, x - 2, y - 1, counter + 1);
+        if(!done) clear(board, counter + 1);
+    }
+    if(!done){
+        solve(board, x + 1, y - 2, counter + 1);
+        if(!done) clear(board, counter + 1);
+    }     
+    if(!done){
+        solve(board, x + 2, y - 1, counter + 1);  
+        if(!done) clear(board, counter + 1);
+    }  
+    if(!done){
+        solve(board, x - 1, y + 2, counter + 1);
+        if(!done) clear(board, counter + 1);
+    }     
+    if(!done){
+        solve(board, x - 2, y + 1, counter + 1);   
+        if(!done) clear(board, counter + 1);
+    } 
+    if(!done){
+        solve(board, x + 1, y + 2, counter + 1);   
+        if(!done) clear(board, counter + 1);
+    }  
+    if(!done){
+        solve(board, x + 2, y + 1, counter + 1);  
+        if(!done) clear(board, counter + 1);
+    } 
+}
